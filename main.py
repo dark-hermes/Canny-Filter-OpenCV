18606909e43b19f34aa01f253ffa7bf57910f429
dark-hermes <hermawansent@gmail.com>
2021-03-31 13:59:58 +0700

    fix typing with PEP8 standard

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import cv2
-import numpy as np
+
 
 # define function to convert captured image into canny edge form
 def canny_filter(image):
-    # grayscaling frame
+    # gray scaling frame
     img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # blur image with bilateralFilter to reduce noise and keep it's sharp edges
-    img_bilateral_blur = cv2.bilateralFilter(img_gray, 9,75,75)
+    img_bilateral_blur = cv2.bilateralFilter(img_gray, 9, 75, 75)
     # use canny algorithm to find the edge
-    canny_edges = cv2.Canny(img_bilateral_blur, 10,45)
+    canny_edges = cv2.Canny(img_bilateral_blur, 10, 45)
     # find the threshold with invert binary
-    _, mask = cv2.threshold(canny_edges, 10,255, cv2.THRESH_BINARY_INV)
-    
+    _, mask = cv2.threshold(canny_edges, 10, 255, cv2.THRESH_BINARY_INV)
+
     return mask
+
 
 # get video capture from webcam
 capture = cv2.VideoCapture(0)
@@ -25,9 +26,8 @@ if user hit enter the program will stopped
 while True:
     ratio, frame = capture.read()
     cv2.imshow('Canny Filter', canny_filter(frame))
-    if cv2.waitKey(1) == 13: # 13 is code for enter key
+    if cv2.waitKey(1) == 13:  # 13 is code for enter key
         break
-        
+
 capture.release()
 cv2.destroyAllWindows()
-
